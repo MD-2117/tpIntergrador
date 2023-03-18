c7ee56b3bb40f2903855effc7024a4cdc0c2b733
MD-2117 <danielivancasas@hotmail.com>
2023-03-17 21:19:33 -0300

    170323 21:19

--- tpIntegrador/src/Main.java
+++ tpIntegrador/src/Main.java
@@ -1,7 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Hello world!");
         int goles = (int)(Math.random()*10+0);
-        System.out.println(goles);
+        String pronostico="C:\\Users\\danie\\OneDrive\\Escritorio\\Argentina Programa\\Repositorio\\tpIntergrador\\pronostico.txt";
+        String ronda="C:\\Users\\danie\\OneDrive\\Escritorio\\Argentina Programa\\Repositorio\\tpIntergrador\\ronda.txt";
+        String[] pronosticoA = leerArchivo(pronostico);
+        String[] rondaA= leerArchivo(ronda);
+        for (int i=0;i<pronosticoA.length;i++) {
+            System.out.println(pronosticoA[i]);
+        }
+        for (int i = 0; i < rondaA.length; i++) {
+            System.out.println(rondaA[i]);
+        }
+    }
+    public static String[] leerArchivo(String ruta) throws FileNotFoundException {
+        File archivo = new File(ruta);
+        Scanner scanner = new Scanner(archivo);
+        String contenido = scanner.nextLine();
+        String[] array = contenido.split(",");
+        scanner.close();
+        return array;
     }
 }
